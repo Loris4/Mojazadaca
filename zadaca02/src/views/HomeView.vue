1df726b43b9abacd85b3d42b26554b4276b24dd5
Loris Lukić <loris.lukic13@gmail.com>
2022-03-24 14:42:08 +0100

    Prvi commit

---
+++ zadaca02/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">Prazna</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  async mounted() {
+    let rezultat = await fetch("https://api.github.com/repos/vuejs/vue/commits");
+    
+    let podaci = await rezultat.json();
+
+    for (let item of podaci) {
+      console.log(item);
+    }
+  },
+};
+</script>
